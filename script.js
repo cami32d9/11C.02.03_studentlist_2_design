c5cc09ce92b0cba33fe7a0e0b6e3e4ec73fa80be
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-09-04 12:42:14 +0200

    Sorting with full name

--- script.js
+++ script.js
@@ -1,32 +1,75 @@
 // ----- INSERT STUDENTS IN LIST -----
 
+document.addEventListener("DOMContentLoaded", start);
+
 let firstname;
 let lastname;
 
-destStudentList = document.querySelector("#student_list");
+const destStudentList = document.querySelector("#student_list");
+const filterButtons = document.querySelectorAll(".filter_button");
+
+function start() {
+  filterButtons.forEach(button => {
+    button.addEventListener("click", function() {
+      let filterBy = this.getAttribute("data-type");
+      insertStudentList(filterBy);
+      document.querySelectorAll("button").forEach(button => {
+        button.classList.remove("button_chosen");
+        this.classList.add("button_chosen");
+      });
+    });
+  });
+
+  getStudentList();
+}
+
+function sortStudents() {
+  students.sort(function(a, b) {
+    var nameA = a.fullname.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.fullname.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
+}
 
 async function getStudentList() {
   let pagesUrl = "students1991.json";
   let jsonData = await fetch(pagesUrl);
-  section = await jsonData.json();
-  insertStudentList();
+  students = await jsonData.json();
+  insertStudentList("all");
 }
 
-function insertStudentList() {
-  section.forEach(section => {
-    let nameArray = `${section.fullname}`.split(" ");
+function insertStudentList(filterBy) {
+  destStudentList.innerHTML = "";
+
+  sortStudents();
+
+  filteredStudents = students.filter(
+    student => filterBy === "all" || student.house === filterBy
+  );
+
+  filteredStudents.forEach(student => {
+    let nameArray = `${student.fullname}`.split(" ");
+
+    student.firstname = nameArray[0];
+    student.lastname = nameArray[1];
 
     let template = `
-                <li class="student ${section.house}" data-firstname="${
+                <li class="student ${student.house}" data-firstname="${
       nameArray[0]
-    }" data-lastname="${nameArray[1]}" data-house="${section.house}">
-                        <h2 class="name">${section.fullname}</h2>
-                        <p class="house">${section.house}</p>
+    }" data-lastname="${nameArray[1]}" data-house="${student.house}">
+                        <h2 class="name">${student.fullname}</h2>
+                        <p class="house">${student.house}</p>
                 </li>
                 `;
 
     destStudentList.insertAdjacentHTML("beforeend", template);
   });
 }
-
-getStudentList();
